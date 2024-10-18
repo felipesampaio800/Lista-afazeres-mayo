ae7ab876842ec357c169e23c1da6643c30f5db22
Felipe <98990688+felipesampaiosilva@users.noreply.github.com>
2024-10-18 14:33:27 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,28 +1,23 @@
 const { Sequelize } = require('sequelize');
 
-// carrega as variáveis de ambiente do arquivo .env
-require('dotenv').config();
-
-// conexão com MySQL
 const sequelize = new Sequelize(
-  process.env.DB_NAME,     
-  process.env.DB_USER,     
-  process.env.DB_PASS,     
+  process.env.DB_NAME,     // Nome do banco de dados
+  process.env.DB_USER,     // Usuário do banco de dados
+  process.env.DB_PASS,     // Senha do banco de dados
   {
-    host: process.env.DB_HOST,   
-    port: process.env.DB_PORT,   
-    dialect: 'mysql',            
-    logging: false               
+    host: process.env.DB_HOST,  // Host do banco de dados
+    port: process.env.DB_PORT,  // Porta do banco de dados (5432 para PostgreSQL)
+    dialect: 'postgres',        // Dialeto correto para PostgreSQL
+    logging: false,             // Desativar logs de SQL (opcional)
   }
 );
 
-// teste conexão com o banco de dados
 sequelize.authenticate()
   .then(() => {
-    console.log('Conexão com o banco de dados MySQL foi bem-sucedida.');
+    console.log('Conexão com o banco de dados foi bem-sucedida.');
   })
   .catch(err => {
-    console.error('Erro ao conectar ao banco de dados:', err);
+    console.error('Erro ao conectar ao banco de dados ou sincronizar tabelas:', err);
   });
 
 module.exports = sequelize;
